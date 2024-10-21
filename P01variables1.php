3519b5debe4db8eef8306c3ef5f268b415a3a744
BrayanYT12 <kingsneakers2812@gmail.com>
2024-10-21 09:12:00 -0600

    Add files via upload

---
+++ P01variables1.php
@@ -0,0 +1,19 @@
+<?php
+$dia = 24; //Se declara variable tipo integer.
+$sueldo = 758.43; //Se declara una variable de tipo double.
+$nombre = " juan"; //Se declara una variable de tipo string.
+$exite = true; //Se declara una variable boolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre; echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "mundo";
+echo $cadena1. "    ".$cadena2;
+?>
