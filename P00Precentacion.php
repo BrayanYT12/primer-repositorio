bb22697b0043f026916954bd2255074366607f23
BrayanYT12 <kingsneakers2812@gmail.com>
2024-10-21 09:31:41 -0600

    Add files via upload

---
+++ P00Precentacion.php
@@ -0,0 +1,18 @@
+<?php 
+/*CBTIS89
+Brayan Daniel Galindo Gonzalez
+3A programacion turno matutino
+*/
+
+
+echo "**precentacion**","<br>","<br>";
+
+echo "Brayan Daniel Galindo Gonzalez";
+echo "<br>";
+echo "Carrera y Grupo:Programacion 3-A";
+echo "<br>";
+echo "6183318048";
+echo "<br>";
+echo "Durango dgo";
+echo "<br>";
+echo "la zapata";
